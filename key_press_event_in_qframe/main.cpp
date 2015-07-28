63e73f510a2fce8674dd137eadc573f5b2d7c18b
Fran Tena <tarod.net@gmail.com>
2015-07-28 13:41:19 +0200

    installed an event filter on the QApplication

--- key_press_event_in_qframe/main.cpp
+++ key_press_event_in_qframe/main.cpp
@@ -1,10 +1,30 @@
 #include <QtWidgets/QApplication>
+#include <QDebug>
 
 #include "myframe.h"
+
+class QApplicationFilter: public QObject
+{
+    public:
+        QApplicationFilter(): QObject() {};
+        ~QApplicationFilter() {};
+
+        bool eventFilter(QObject* obj, QEvent* event)
+        {
+            qDebug() << "QApplicationFilter: "
+                     << obj->objectName()
+                     << " - event type: "
+                     << event->type();
+	    return QObject::eventFilter(obj, event);
+        };
+};
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    a.installEventFilter(new QApplicationFilter());
+
     MyFrame mf;
     mf.show();
     return a.exec();
