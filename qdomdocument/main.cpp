cccec7c705a1eb9bba8d0beef68e511c6c1c8318
Tarod <tarod.net@gmail.com>
2016-04-13 15:22:25 +0200

    code to test a fail case

--- qdomdocument/main.cpp
+++ qdomdocument/main.cpp
@@ -17,11 +17,25 @@ int main()
 
     int line, column;
 
-    if(dom.setContent(&file, &error, &line, &column)){
-        qDebug() << dom.toString(4);
+    if(dom.setContent(&file, &error, &line, &column)) {
+        qDebug() << "Content: " << dom.toString(4);
     } else {
         qDebug() << "Error:" << error << "in line " << line << "column" << column;
     }
+
+    file.close();
+    file.open(QFile::ReadOnly|QFile::Text);
+
+    QTextStream in(&file);
+    in.readAll();
+
+    //This will fail because we have reached the end of file.
+    if(dom.setContent(in.readAll(), &error, &line, &column)) {
+        qDebug() << "Content: " << dom.toString(4);
+    } else {        
+        qDebug() << "Error:" << error << "in line " << line << "column" << column;
+    }
+
     return 0;
 }
 
