be2d0bb944b9e45ac0172bcc1a35ec6490bde9a8
Fran Tena <tarod.net@gmail.com>
2021-04-30 18:09:15 +0200

    new example - sorting qtableview

---
+++ models-sorting-qtableview/widget.h
@@ -0,0 +1,32 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QStandardItemModel>
+
+class QSortFilterProxyModel;
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_checkBox_toggled(bool checked);
+
+private:
+    Ui::Widget *ui;
+
+    QStandardItemModel *model;
+    QStandardItemModel *model_2;
+    QSortFilterProxyModel *proxy_model;
+};
+
+#endif // WIDGET_H
