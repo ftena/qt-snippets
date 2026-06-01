b7c66b242f9661730c027a2f1c12e89a81a0a913
Fran Tena <tarod.net@gmail.com>
2026-06-01 15:12:33 +0200

    update code, qrc and cmake to qt 6

--- built_in_translation/main.cpp
+++ built_in_translation/main.cpp
@@ -6,13 +6,12 @@
 
 int main(int argc, char *argv[])
 {
-
 	QApplication app(argc, argv);
 
 	QTranslator qtTranslator;
 	if (qtTranslator.load(QLocale::system(),
 				"qt", "_",
-				QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+                QLibraryInfo::path(QLibraryInfo::TranslationsPath)))
 	{
 		qDebug() << "qtTranslator ok";
 		app.installTranslator(&qtTranslator);
@@ -20,18 +19,26 @@ int main(int argc, char *argv[])
 
 	QTranslator qtBaseTranslator;
 	if (qtBaseTranslator.load("qtbase_" + QLocale::system().name(),
-				QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+                QLibraryInfo::path(QLibraryInfo::TranslationsPath)))
 	{
 		qDebug() << "qtBaseTranslator ok";
 		app.installTranslator(&qtBaseTranslator);
 	}
 
+    QString path = QApplication::applicationDirPath();
+    qDebug() << "app path: " << QApplication::applicationDirPath();
+
+    path.append("/res/translations/");
+
 	QTranslator translator;
-	translator.load("myapp_es", ":/");
-	app.installTranslator(&translator);
+    if (translator.load(path + "translation_es.qm"))
+    {
+        qDebug() << "translator ok";
+        app.installTranslator(&translator);
+    }
 
 	QMessageBox::question(0, QObject::tr("Sure want to quit?"),
-				QObject::tr("Sure?"),
+                QObject::tr("Sure?"),
 				QMessageBox::Yes | QMessageBox::No);
 
 	return app.exec();
---
+++ built_in_translation/res/translations/translation_es.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+Run the update_translations CMake target to populate the source strings in this file.
+-->
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Sure want to quit?</source>
+        <translation>¿Estás seguro de querer salir?</translation>
+    </message>
+    <message>
+        <source>Sure?</source>
+        <translation>¿Seguro?</translation>
+    </message>
+</context>
+</TS>
