8686a04abd5d3e2372695c6fd580f8f6e1ad17e0
Fran Tena <tarod.net@gmail.com>
2026-06-07 11:44:54 +0200

    update code to qt6

--- event-filters/mywindow.cpp
+++ event-filters/mywindow.cpp
@@ -7,8 +7,8 @@ MyWindow::MyWindow()
 {
     imageLabel = new QLabel(this);
     imageLabel->setPixmap(QPixmap(":icon"));
-    imageLabel->resize(imageLabel->pixmap()->size());
-    qDebug() << imageLabel->pixmap()->size();
+    imageLabel->resize(imageLabel->pixmap().size());
+    qDebug() << imageLabel->pixmap().size();
 
     imageLabel->setMouseTracking(true);
     imageLabel->installEventFilter(this);
@@ -25,12 +25,12 @@ bool MyWindow::eventFilter(QObject *object, QEvent *event)
          * We know the image is 60x60 px.
          */
         QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-        if(mouseEvent->x() >= 0 && mouseEvent->x() <=30 &&
-           mouseEvent->y() >= 0 && mouseEvent->y() <= 30)
+        if(mouseEvent->position().x() >= 0 && mouseEvent->position().x() <=30 &&
+            mouseEvent->position().y() >= 0 && mouseEvent->position().y() <= 30)
         {
-            qDebug() << "drawFrame()";
+            qDebug() << " >=0 x <= 30 && >= y <= 30 ";
         } else {
-            qDebug() << "!drawFrame()";
+            qDebug() << " a diffrent path ";
         }
         return false;
     }
